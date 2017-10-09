d6f5c836d5ff20abfb875adcd892cf6a49926115
reedsa <stuart.reed@cognizant.com>
2017-10-09 16:06:49 -0600

    Simplify playerHasWon logic

--- src/utils/playerHasWon.js
+++ src/utils/playerHasWon.js
@@ -6,39 +6,20 @@ export function playerHasWon(gameState, player) {
     return false; // Draw
   }
 
-  const winCount = gameState.winCondition[0];
+  const directions = [
+    { x: 0, y: 1 },
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: -1, y: 1 },
+  ];
 
-  // Find a vertical sequence for player
   for (let col = 0; col < gameState.boardHeights.length; col++) {
     for (let row = 0; row < gameState.boardHeights[col]; row++) {
-      if (checkWin(gameState, player, col, row, { x: 0, y: 1 })) {
-        return true;
-      }
-    }
-  }
+      const winningSequence = directions.some(direction =>
+        checkWin(gameState, player, col, row, direction)
+      );
 
-  // Find a horizontal sequence for player
-  for (let col = 0; col < gameState.boardHeights.length; col++) {
-    for (let row = 0; row < gameState.boardHeights[col]; row++) {
-      if (checkWin(gameState, player, col, row, { x: 1, y: 0 })) {
-        return true;
-      }
-    }
-  }
-
-  // Find a diagonal up sequence for player
-  for (let col = 0; col < gameState.boardHeights.length; col++) {
-    for (let row = 0; row < gameState.boardHeights[col]; row++) {
-      if (checkWin(gameState, player, col, row, { x: 1, y: 1 })) {
-        return true;
-      }
-    }
-  }
-
-  // Find a diagonal down sequence for player
-  for (let col = 0; col < gameState.boardHeights.length; col++) {
-    for (let row = winCount - 1; row < gameState.boardHeights[col]; row++) {
-      if (checkWin(gameState, player, col, row, { x: 1, y: -1 })) {
+      if (winningSequence) {
         return true;
       }
     }
