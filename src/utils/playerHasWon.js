01947a08afe3281686700ea48b46e7acda8f20e1
reedsa <stuart.reed@cognizant.com>
2017-10-09 15:36:01 -0600

    Remove optimization for checking win states

--- src/utils/playerHasWon.js
+++ src/utils/playerHasWon.js
@@ -8,44 +8,36 @@ export function playerHasWon(gameState, player) {
 
   const winCount = gameState.winCondition[0];
 
-  // Vertical rows
-  const verticalCols = gameState.boardHeights.length;
-  for (let col = 0; col < verticalCols; col++) {
-    const verticalRows = gameState.boardHeights[col] - (winCount - 1);
-    for (let row = 0; row < verticalRows; row++) {
+  // Find a vertical sequence for player
+  for (let col = 0; col < gameState.boardHeights.length; col++) {
+    for (let row = 0; row < gameState.boardHeights[col]; row++) {
       if (checkWin(gameState, player, col, row, { x: 0, y: 1 })) {
         return true;
       }
     }
   }
 
-  // Horizontal rows
-  const horizontalCols = gameState.boardHeights.length - (winCount - 1);
-  for (let col = 0; col < horizontalCols; col++) {
-    const horizontalRows = gameState.boardHeights[col];
-    for (let row = 0; row < horizontalRows; row++) {
+  // Find a horizontal sequence for player
+  for (let col = 0; col < gameState.boardHeights.length; col++) {
+    for (let row = 0; row < gameState.boardHeights[col]; row++) {
       if (checkWin(gameState, player, col, row, { x: 1, y: 0 })) {
         return true;
       }
     }
   }
 
-  // Diagonal Up
-  const diagonalUpCols = gameState.boardHeights.length - (winCount - 1);
-  for (let col = 0; col < diagonalUpCols; col++) {
-    const diagonalUpRows = gameState.boardHeights[col] - (winCount - 1);
-    for (let row = 0; row < diagonalUpRows; row++) {
+  // Find a diagonal up sequence for player
+  for (let col = 0; col < gameState.boardHeights.length; col++) {
+    for (let row = 0; row < gameState.boardHeights[col]; row++) {
       if (checkWin(gameState, player, col, row, { x: 1, y: 1 })) {
         return true;
       }
     }
   }
 
-  // Diagonal Down
-  const diagonalDownCols = gameState.boardHeights.length - (winCount - 1);
-  for (let col = 0; col < diagonalDownCols; col++) {
-    const diagonalDownRows = gameState.boardHeights[col];
-    for (let row = winCount - 1; row < diagonalDownRows; row++) {
+  // Find a diagonal down sequence for player
+  for (let col = 0; col < gameState.boardHeights.length; col++) {
+    for (let row = winCount - 1; row < gameState.boardHeights[col]; row++) {
       if (checkWin(gameState, player, col, row, { x: 1, y: -1 })) {
         return true;
       }
